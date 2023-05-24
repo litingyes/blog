0b388ab503b184389a2e70b8a5ba79eefee20166
liting <luz.liting@gmail.com>
2023-05-24 19:06:33 +0800

    chore(theme): set bg color

--- app.vue
+++ app.vue
@@ -38,7 +38,7 @@ function switchTheme(e: MouseEvent) {
 </script>
 
 <template>
-  <div class="default-layout w-screen h-screen">
+  <div class="default-layout w-screen h-screen bg-slate-50 dark:bg-slate-800">
     <nav class="h-16 border-solid border-gray-1 flex justify-end items-center px-8 border-b-2">
       <span v-show="$colorMode.value === 'light'" class="text-2xl cursor-pointer i-ic:outline-light-mode" @click="switchTheme" />
       <span v-show="$colorMode.value === 'dark'" class="text-2xl cursor-pointer i-ic:outline-dark-mode" @click="switchTheme" />
