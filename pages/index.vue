509f5154d5935ae6088e8aa873159efa0f8f36d6
liting <luz.liting@gmail.com>
2023-05-27 00:06:40 +0800

    feat(home page): hide img when width < sm size

--- pages/index.vue
+++ pages/index.vue
@@ -2,36 +2,36 @@
 </script>
 
 <template>
-  <div id="page-index" class="flex items-center justify-center w-screen h-full relative">
-    <div class="flex justify-center sm:w-4/5 md:3/5 overflow-hidden bg-slate-200 hover:scale-[1.01] transition-transform drop-shadow-lg dark:bg-slate-700 rounded-xl lg:w-192 w-9/10">
-      <NuxtImg class="w-1/2" src="/images/homePersonal.jpg" sizes="sm:40vw md:288 lg:384" />
-      <div class="w-1/2 lg:p-8 md:p-6 sm:p-4 p-2 prose">
-        <div class="text-center font-semibold text-xl">
+  <div id="page-index" class="relative h-full w-screen flex items-center justify-center overflow-y-auto">
+    <div class="md:3/5 mt-4 w-9/10 flex justify-center overflow-hidden rounded-xl bg-slate-200 drop-shadow-lg transition-transform lg:w-192 sm:w-4/5 hover:scale-[1.01] dark:bg-slate-700">
+      <NuxtImg class="hidden sm:block sm:w-1/2" src="/images/homePersonal.jpg" sizes="sm:40vw md:288 lg:384" />
+      <div class="p-2 prose sm:w-1/2 lg:p-8 md:p-6 sm:p-4">
+        <div class="text-center text-xl font-semibold">
           文侯
         </div>
-        <ul class="flex justify-center !list-none !p-0 h-8 sm:gap-1">
+        <ul class="h-8 flex justify-center !list-none sm:gap-1 !p-0">
           <NuxtLink to="https://github.com/liting-yes" target="__black">
-            <li class="btn-container h-8 w-8 p-0 m-0 hover:bg-slate-300 dark:hover:bg-slate-600">
+            <li class="m-0 h-8 w-8 p-0 hover:bg-slate-300 btn-container dark:hover:bg-slate-600">
               <span class="i-line-md:github-loop" />
             </li>
           </NuxtLink>
           <NuxtLink to="https://juejin.cn/user/2217072231984919" target="__black">
-            <li class="btn-container h-8 w-8 p-0 m-0 hover:bg-slate-300 dark:hover:bg-slate-600">
+            <li class="m-0 h-8 w-8 p-0 hover:bg-slate-300 btn-container dark:hover:bg-slate-600">
               <span class="i-tabler:brand-juejin" />
             </li>
           </NuxtLink>
           <NuxtLink to="https://space.bilibili.com/472116791" target="__black">
-            <li class="btn-container h-8 w-8 p-0 m-0 hover:bg-slate-300 dark:hover:bg-slate-600">
+            <li class="m-0 h-8 w-8 p-0 hover:bg-slate-300 btn-container dark:hover:bg-slate-600">
               <span class="i-custom:blibli" />
             </li>
           </NuxtLink>
           <NuxtLink to="https://twitter.com/liting_yes" target="__black">
-            <li class="btn-container h-8 w-8 p-0 m-0 hover:bg-slate-300 dark:hover:bg-slate-600">
+            <li class="m-0 h-8 w-8 p-0 hover:bg-slate-300 btn-container dark:hover:bg-slate-600">
               <span class="i-ri:twitter-line" />
             </li>
           </NuxtLink>
           <UTooltip text="2972188187">
-            <li class="btn-container h-8 w-8 p-0 m-0 hover:bg-slate-300 dark:hover:bg-slate-600">
+            <li class="m-0 h-8 w-8 p-0 hover:bg-slate-300 btn-container dark:hover:bg-slate-600">
               <span class="i-ri:qq-line" />
             </li>
           </UTooltip>
