6f3b1c0b18cb767788a8e92f96a4734311bbdfc6
liting <luz.liting@gmail.com>
2023-05-27 23:38:05 +0800

    chore: add NuxtLoadingIndicator

--- app.vue
+++ app.vue
@@ -55,6 +55,7 @@ function switchTheme(e: MouseEvent) {
       </div>
     </nav>
     <main class="nuxt-app__main overflow-y-auto">
+      <NuxtLoadingIndicator />
       <NuxtPage />
     </main>
   </div>
