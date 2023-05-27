360b9b9c75713759c6dd83e44825b61d4c7a282e
liting <luz.liting@gmail.com>
2023-05-28 00:34:03 +0800

    fix(plugin): determine if file.layout is undefined

--- server/plugins/content.ts
+++ server/plugins/content.ts
@@ -1,6 +1,6 @@
 export default defineNitroPlugin((nitroApp) => {
   nitroApp.hooks.hook('content:file:afterParse', (file) => {
-    if (!file._id.endsWith('.md') || file.layout !== 'default')
+    if (!file._id.endsWith('.md') || (file.layout && file.layout !== 'default'))
       return
     file.layout = 'article'
   })
