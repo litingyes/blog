e840e657d9d4722afe411e6714c37794c5d440d5
liting <luz.liting@gmail.com>
2023-05-28 10:54:02 +0800

    feat(layout): use prose style in article layout

--- layouts/article.vue
+++ layouts/article.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="article-layout flex justify-between">
     <div class="hidden w-48 sm:block" />
-    <ContentDoc class="w-full px-8 py-4 sm:flex-1" />
+    <ContentDoc class="w-full px-8 py-4 prose sm:flex-1" />
     <div class="hidden w-48 sm:block" />
   </div>
 </template>
