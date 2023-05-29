bcf7419d69ddc0a43d04c6e98d8c8b1715b7f0b0
liting <luz.liting@gmail.com>
2023-05-29 16:19:58 +0800

    feat(content): add smooth scroll

--- layouts/article.vue
+++ layouts/article.vue
@@ -1,16 +1,21 @@
-import { content } from '../.nuxt/tailwind.config';
 <script setup lang="ts">
+function scrollSmooth(id: string) {
+  document?.querySelector(`#${id}`)?.scrollIntoView({
+    behavior: 'smooth',
+    block: 'start',
+  })
+}
 </script>
 
 <template>
   <div class="article-layout relative">
     <ContentDoc v-slot="{ doc }">
       <div class="absolute left-0 top-0 hidden w-48 sm:block" />
-      <div class="scrollbar-primary absolute inset-0 h-full scroll-py-2">
+      <div class="scrollbar-primary absolute inset-0 h-full scroll-py-2 scroll-smooth">
         <ContentRenderer class="mx-auto px-8 py-4 prose prose-slate dark:prose-invert" :value="doc" />
       </div>
       <div class="absolute right-0 top-0 hidden w-64 flex-col py-8 md:flex">
-        <a v-for="link in doc?.body?.toc?.links" :key="link.id" class="overflow-hidden text-ellipsis px-2 py-1" :href="`#${link.id}`">
+        <a v-for="link in doc?.body?.toc?.links" :key="link.id" class="overflow-hidden text-ellipsis px-2 py-1" :href="`#${link.id}`" @click.prevent="scrollSmooth(link.id)">
           <span class="relative inline-block after:content-['']" after="absolute inset-y-0 left-0 w-0 transition-all hover:(border-b-2 w-full) border-slate-500">{{ link.text }}</span>
         </a>
       </div>
