584444b030ad68895b41176d1b78417314da003a
liting <luz.liting@gmail.com>
2023-05-29 22:52:34 +0800

    perf(content): set default value of target to __blank in prose-a sfc

---
+++ components/content/ProseA.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+defineProps({
+  href: {
+    type: String,
+    default: '',
+  },
+  target: {
+    type: String,
+    default: '__blank',
+    required: false,
+  },
+})
+</script>
+
+<template>
+  <NuxtLink :href="href" :target="target">
+    <slot />
+  </NuxtLink>
+</template>
