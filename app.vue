4af071331023a4d774c03aa04cd2e6cd8c258d59
liting <luz.liting@gmail.com>
2023-05-29 20:31:38 +0800

    fix(theme): display icon when first load

--- app.vue
+++ app.vue
@@ -46,12 +46,10 @@ function switchTheme(e: MouseEvent) {
   <div class="nuxt-app h-screen w-screen bg-slate-50 dark:bg-slate-800">
     <nav class="h-16 flex items-center justify-end border-b-2 border-gray-1 border-solid px-8 dark:border-slate-700">
       <div ref="themeIconRef" class="h-9 w-9 flex-col btn-container" @click="switchTheme">
-        <Transition name="list">
-          <span v-show="displayLightIcon" class="i-ic:outline-light-mode text-2xl" />
-        </Transition>
-        <Transition name="list">
-          <span v-show="!displayLightIcon" class="i-ic:outline-dark-mode text-2xl" />
-        </Transition>
+        <TransitionGroup name="list">
+          <span v-if="displayLightIcon" key="light" class="i-ic:outline-light-mode text-2xl" />
+          <span v-else key="dark" class="i-ic:outline-dark-mode text-2xl" />
+        </TransitionGroup>
       </div>
     </nav>
     <main class="nuxt-app__main overflow-y-auto" scrollbar="~ rounded track-color-slate-200 dark:track-color-slate-700 thumb-color-slate-300 dark:thumb-color-slate-600">
