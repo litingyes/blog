81b9a04eed4d55304cb35cf60012ad2ba2115b81
liting <luz.liting@gmail.com>
2024-06-15 23:20:32 +0800

    feat: add nextui

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,16 @@
+const { nextui } = require('@nextui-org/react')
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
     '.src/**/*.{tsx,mdx}',
+    './node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}',
   ],
   theme: {
     extend: {},
   },
-  plugins: [],
+  plugins: [
+    nextui(),
+  ],
+  darkMode: 'class',
 }
