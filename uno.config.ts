79fc1af662c85a52fad85e9b7cc75a3ffbadf33f
liting <luz.liting@gmail.com>
2024-06-15 17:56:39 +0800

    chore: add unocss

---
+++ uno.config.ts
@@ -0,0 +1,9 @@
+import { defineConfig, presetIcons, presetUno, transformerDirectives, transformerVariantGroup } from 'unocss'
+
+export default defineConfig({
+  presets: [
+    presetUno(),
+    presetIcons(),
+  ],
+  transformers: [transformerVariantGroup(), transformerDirectives()],
+})
