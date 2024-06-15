2dc8c437c9c226871fbaab72d2250184fc3a570d
liting <luz.liting@gmail.com>
2024-06-15 23:15:28 +0800

    feat: replace unocss with tailwindcss

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,21 +1,25 @@
 import antfu from '@antfu/eslint-config'
 import next from '@next/eslint-plugin-next'
+import tailwind from 'eslint-plugin-tailwindcss'
 
-export default antfu({
-  javascript: true,
-  typescript: true,
-  jsx: true,
-  jsonc: true,
-  unocss: true,
-  react: true,
-  regexp: true,
-  formatters: {
-    css: true,
-    html: true,
-    markdown: true,
+export default antfu(
+  {
+    javascript: true,
+    typescript: true,
+    jsx: true,
+    jsonc: true,
+    react: true,
+    regexp: true,
+    formatters: {
+      css: true,
+      html: true,
+      markdown: true,
+    },
   },
-}, {
-  plugins: {
-    next,
+  {
+    plugins: [
+      next,
+      tailwind,
+    ],
   },
-})
+)
--- uno.config.ts
+++
@@ -1,9 +0,0 @@
-import { defineConfig, presetIcons, presetUno, transformerDirectives, transformerVariantGroup } from 'unocss'
-
-export default defineConfig({
-  presets: [
-    presetUno(),
-    presetIcons(),
-  ],
-  transformers: [transformerVariantGroup(), transformerDirectives()],
-})
