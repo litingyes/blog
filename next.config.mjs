5d9fe4c35f5206bdb591af8f7e2ef445060dc547
liting <luz.liting@gmail.com>
2024-06-19 23:28:10 +0800

    feat(mdx): add remark plugins

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,14 @@
 import withMDX from '@next/mdx'
+import remarkFrontmatter from 'remark-frontmatter'
+import remarkMdxFrontmatter from 'remark-mdx-frontmatter'
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   pageExtensions: ['js', 'jsx', 'mdx', 'ts', 'tsx'],
 }
 
-export default withMDX()(nextConfig)
+export default withMDX({
+  options: {
+    remarkPlugins: [remarkFrontmatter, remarkMdxFrontmatter],
+  },
+})(nextConfig)
