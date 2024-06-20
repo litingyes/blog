ea18026cb7da29569d1c26fd0c2024213ea0c964
liting <luz.liting@gmail.com>
2024-06-21 00:19:31 +0800

    fix(tailwind): content path error

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -3,8 +3,7 @@ import { nextui } from '@nextui-org/react'
 
 const config: Config = {
   content: [
-    '.src/app/**/*.{js,ts,jsx,tsx,mdx}',
-    '.src/components/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/**/*.{js,ts,jsx,tsx,mdx}',
     './node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}',
   ],
   theme: {
