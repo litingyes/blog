02863236c147d08f94ceda4aadeb22c360e82a23
liting <luz.liting@gmail.com>
2024-07-18 22:16:57 +0800

    feat(about): init

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,9 +1,21 @@
+import { Avatar, Chip } from '@nextui-org/react'
+import Link from 'next/link'
 import { IconWork } from '@/components/icons/work'
 import { IconZoom } from '@/components/icons/zoom'
 import { transformDateStringToDisplayDate } from '@/utils/date'
+import { getAllMdxData } from '@/utils/mdx'
+import { IconDateRange } from '@/components/icons/dateRange'
+import { IconGithub } from '@/components/icons/github'
+
+const SOCIALS = [
+  {
+    label: 'Github',
+    link: 'https://github.com/litingyes',
+    icon: <IconGithub />,
+  },
+]
 
 const PRESENT = 'present'
-
 const WORKS = [
   {
     company: 'ZOOM',
@@ -14,38 +26,82 @@ const WORKS = [
   },
 ]
 
-export default function AboutPage() {
+export default async function AboutPage() {
+  const posts = await getAllMdxData()
+
   return (
-    <div className="px-8">
+    <div className="px-8 pt-16">
       <div className="grid grid-cols-2 gap-8">
         <div>
-        </div>
-        <div className="rounded-xl border border-solid border-gray-200 p-4">
-          <div className="flex items-center gap-2 font-semibold text-slate-600">
-            <IconWork />
-            <span>Work experience</span>
+          <div>
+            <Avatar src="/avatar.jpeg" radius="lg" className="!size-36 text-large hover:animate-spin" name="litingyes" />
+            <section className="mt-8 text-xl text-slate-600">
+              <p>Hi! I'm Luz Liu, a software engineer based in China.</p>
+            </section>
+            <ul className="mt-8">
+              {SOCIALS.map(item => (
+                <li key={item.label}>
+                  <Link href={item.link} target="_blank" className="flex cursor-pointer items-center gap-2 text-xl  text-slate-600">
+                    <i className="text-2xl">{item.icon}</i>
+                    {`follow on ${item.label}`}
+                  </Link>
+                </li>
+              ))}
+            </ul>
           </div>
-          <ul className="mt-4">
-            {WORKS.map(work => (
-              <li className="flex items-center gap-4" key={work.onboarding}>
-                <i className="text-4xl">
-                  {work.icon}
-                </i>
-                <div className="flex-1">
-                  <div className="flex items-center justify-between">
-                    <span className="text-base font-semibold text-slate-950">{work.company}</span>
-                    <span className="flex items-center gap-2 text-sm text-slate-500">
-                      <span>{work.onboarding}</span>
-                      <span>-</span>
-                      <span>{work.resignation}</span>
-                    </span>
+          <div className="mt-20 rounded-xl border border-solid border-gray-200 p-4">
+            <div className="flex items-center gap-2 font-semibold text-slate-600">
+              <IconWork />
+              <span>Work experience</span>
+            </div>
+            <ul className="mt-4">
+              {WORKS.map(work => (
+                <li className="flex items-center gap-4" key={work.onboarding}>
+                  <i className="text-4xl">
+                    {work.icon}
+                  </i>
+                  <div className="flex-1">
+                    <div className="flex items-center justify-between">
+                      <span className="text-base font-semibold text-slate-950">{work.company}</span>
+                      <span className="flex items-center gap-2 text-sm text-slate-500">
+                        <span>{work.onboarding}</span>
+                        <span>-</span>
+                        <span>{work.resignation}</span>
+                      </span>
+                    </div>
+                    <div className="text-sm text-slate-600">{work.position}</div>
                   </div>
-                  <div className="text-sm text-slate-600">{work.position}</div>
+                </li>
+              ))}
+            </ul>
+          </div>
+        </div>
+        <div>
+          <ul className="flex flex-col gap-10 border-l-1 border-solid border-gray-300">
+            {posts.map(post => (
+              <li className="flex" key={post.slug}>
+                <div className="w-56 pl-4">
+                  <div className="flex items-center gap-1 text-slate-400">
+                    <i className="text-xl">
+                      <IconDateRange />
+                    </i>
+                    <span>{transformDateStringToDisplayDate(post.frontmatter.lastUpdatedTime as string)}</span>
+                  </div>
+                </div>
+                <div>
+                  <h2 className="text-2xl font-semibold text-slate-800 transition-colors hover:text-blue-600">
+                    <Link href={`/posts/${post.slug}`}>{post.frontmatter.title as string}</Link>
+                  </h2>
+                  <p className="mt-1 text-base text-slate-600">{post.frontmatter.description as string}</p>
+                  <div className="mt-2">
+                    { (post.frontmatter.tags as string[])?.map(tag => (<Chip key={tag} size="sm" radius="sm">{tag}</Chip>))}
+                  </div>
                 </div>
               </li>
             ))}
           </ul>
         </div>
+
       </div>
     </div>
   )
--- src/components/icons/github.tsx
+++ src/components/icons/github.tsx
@@ -1,24 +1,7 @@
-export function GithubIcon(props: Record<string, unknown>) {
+export function IconGithub() {
   return (
-    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" {...props}>
-      <mask id="lineMdGithubLoop0" width={24} height={24} x={0} y={0}>
-        <g fill="#fff">
-          <ellipse cx={9.5} cy={9} rx={1.5} ry={1}></ellipse>
-          <ellipse cx={14.5} cy={9} rx={1.5} ry={1}></ellipse>
-        </g>
-      </mask>
-      <g fill="none" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth={2}>
-        <path strokeDasharray={30} strokeDashoffset={30} d="M12 4C13.6683 4 14.6122 4.39991 15 4.5C15.5255 4.07463 16.9375 3 18.5 3C18.8438 4 18.7863 5.21921 18.5 6C19.25 7 19.5 8 19.5 9.5C19.5 11.6875 19.017 13.0822 18 14C16.983 14.9178 15.8887 15.3749 14.5 15.5C15.1506 16.038 15 17.3743 15 18C15 18.7256 15 21 15 21M12 4C10.3317 4 9.38784 4.39991 9 4.5C8.47455 4.07463 7.0625 3 5.5 3C5.15625 4 5.21371 5.21921 5.5 6C4.75 7 4.5 8 4.5 9.5C4.5 11.6875 4.98301 13.0822 6 14C7.01699 14.9178 8.1113 15.3749 9.5 15.5C8.84944 16.038 9 17.3743 9 18C9 18.7256 9 21 9 21">
-          <animate fill="freeze" attributeName="stroke-dashoffset" dur="0.6s" values="30;0"></animate>
-        </path>
-        <path strokeDasharray={10} strokeDashoffset={10} d="M9 19">
-          <animate fill="freeze" attributeName="stroke-dashoffset" begin="0.7s" dur="0.2s" values="10;0"></animate>
-          <animate attributeName="d" dur="3s" repeatCount="indefinite" values="M9 19c-1.406 0-2.844-.563-3.688-1.188C4.47 17.188 4.22 16.157 3 15.5;M9 19c-1.406 0-3-.5-4-.5-.532 0-1 0-2-.5;M9 19c-1.406 0-2.844-.563-3.688-1.188C4.47 17.188 4.22 16.157 3 15.5"></animate>
-        </path>
-      </g>
-      <rect width={8} height={4} x={8} y={11} fill="currentColor" mask="url(#lineMdGithubLoop0)">
-        <animate attributeName="y" dur="3s" keyTimes="0;0.45;0.46;0.54;0.55;1" repeatCount="indefinite" values="11;11;7;7;11;11"></animate>
-      </rect>
+    <svg xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" viewBox="0 0 1024 1024">
+      <path fill="currentColor" d="M511.6 76.3C264.3 76.2 64 276.4 64 523.5C64 718.9 189.3 885 363.8 946c23.5 5.9 19.9-10.8 19.9-22.2v-77.5c-135.7 15.9-141.2-73.9-150.3-88.9C215 726 171.5 718 184.5 703c30.9-15.9 62.4 4 98.9 57.9c26.4 39.1 77.9 32.5 104 26c5.7-23.5 17.9-44.5 34.7-60.8c-140.6-25.2-199.2-111-199.2-213c0-49.5 16.3-95 48.3-131.7c-20.4-60.5 1.9-112.3 4.9-120c58.1-5.2 118.5 41.6 123.2 45.3c33-8.9 70.7-13.6 112.9-13.6c42.4 0 80.2 4.9 113.5 13.9c11.3-8.6 67.3-48.8 121.3-43.9c2.9 7.7 24.7 58.3 5.5 118c32.4 36.8 48.9 82.7 48.9 132.3c0 102.2-59 188.1-200 212.9a127.5 127.5 0 0 1 38.1 91v112.5c.8 9 0 17.9 15 17.9c177.1-59.7 304.6-227 304.6-424.1c0-247.2-200.4-447.3-447.5-447.3"></path>
     </svg>
   )
 }
